c61067f65ed2ca929725b9eeeaa6318221aa9f94
cheng <cheng>
2019-09-12 21:30:57 +0800

    test

---
+++ Index.php
@@ -0,0 +1,4 @@
+<?php
+require('./vendor/autoload.php');
+use demo\Index;
+Index::index(); 
---
+++ demo/Index.php
@@ -0,0 +1,11 @@
+<?php
+namespace demo;
+/**
+	 * 
+	 */
+class Index
+{
+	public static function index(){
+		echo 3333;
+	}
+}	
